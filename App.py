0edbc5156fd902a2bdf352eb6fa52115ce227763
heitorrapela <hrm@cin.ufpe.br>
2016-08-13 20:55:26 -0300

    Versao pegando os valores json dos sensores definidos

--- App.py
+++ App.py
@@ -1,6 +1,17 @@
-from Sensor import getTempData
+from Sensor import getEmbarqueLab
 from flask import Flask, request, render_template, url_for
 import json
-urlSensor = "http://172.19.5.253/embarquelab/downloads/EL_sensores_json.php"
 
-print getTempData(urlSensor)
+# 6 - temp 7 - umidade 9 - luminosidade 17 - Pressao do ar
+listaSensores = []
+sensorAcesso = [6,7,9,17]
+
+for x in sensorAcesso:
+	retorno =  getEmbarqueLab(x)
+	listaSensores.append(retorno)
+
+#for x in listaSensores:
+#	print x[0]
+#	print x[1]
+#	print x[2]
+#	print "'''"
--- Sensor.py
+++ Sensor.py
@@ -3,9 +3,11 @@ from urllib import urlopen
 from pprint import pprint
 #url = "http://172.19.5.253/embarquelab/downloads/EL_sensores_json.php"
 
-def getTempData(url):
-    jsondata = urlopen(url)
-    data = json.loads(jsondata.read())
-    temperatura = data["Sensor"][15]["Valor"]
-    #print temperatura
-    return temperatura
+def getEmbarqueLab(i):
+	urlSensor = "http://172.19.5.253/embarquelab/downloads/EL_sensores_json.php"
+	jsondata = urlopen(urlSensor)
+	data = json.loads(jsondata.read())
+	nome = data["Sensor"][i-1]["Nome"]
+	descricao = data["Sensor"][i-1]["Descricao"]
+	valor = data["Sensor"][i-1]["Valor"]
+	return nome, descricao, valor
