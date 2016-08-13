9f4d83d1e3ef19c8b0de3b869dbd9d34ba479b7f
dangalf <dams@cin.ufpe.br>
2016-08-13 19:23:30 -0300

    Banco de dados criado

--- App.py
+++ App.py
@@ -1,4 +1,6 @@
 from Sensor import getTempData
+from flask import Flask, request, render_template, url_for
+import json
 urlSensor = "http://172.19.5.253/embarquelab/downloads/EL_sensores_json.php"
 
 print getTempData(urlSensor)
