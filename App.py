0b463d4c4afa1c70720b5e22376fff47caafe191
dangalf <dams@cin.ufpe.br>
2016-08-14 01:13:57 -0300

    Conflito de merge

--- App.py
+++ App.py
@@ -1,12 +1,30 @@
-from Sensor import getTempData
+from Sensor import getEmbarqueLab
+from Sensor import getParquesPracas
 from flask import Flask, request, render_template, url_for
 import json
-import datetime
-
-urlSensor = "http://172.19.5.253/embarquelab/downloads/EL_sensores_json.php"
-
-#print getTempData(urlSensor)
-
-app = Flask(__name__)
 
 
+##################DADOS SENSORES##########################
+# 6 - temp 7 - umidade 9 - luminosidade 17 - Pressao do ar
+listaSensores = []
+sensorAcesso = [6,7,9,17]
+
+for x in sensorAcesso:
+	retorno =  getEmbarqueLab(x)
+	listaSensores.append(retorno)
+
+#for x in listaSensores:
+#	print x[0]
+#	print x[1]
+#	print x[2]
+#	print "'''"
+
+
+###################DADOS PARQUES PRACAS#################
+# 498 = Quantidade de pracas ou parques no json
+x = getParquesPracas(498)
+
+#for i in range(0,498):
+#	print x[i]
+
+###################DADOS PARQUES PRACAS#################
--- Sensor.py
+++ Sensor.py
@@ -3,11 +3,48 @@ from urllib import urlopen
 from pprint import pprint
 #url = "http://172.19.5.253/embarquelab/downloads/EL_sensores_json.php"
 
+def getEmbarqueLab(i):
+	urlSensor = "http://172.19.5.253/embarquelab/downloads/EL_sensores_json.php"
+	jsondata = urlopen(urlSensor)
+	data = json.loads(jsondata.read())
+	nome = data["Sensor"][i-1]["Nome"]
+	descricao = data["Sensor"][i-1]["Descricao"]
+	valor = data["Sensor"][i-1]["Valor"]
+	return nome, descricao, valor
 
+def getParquesPracas(j):
+	urlSensor = "http://dados.recife.pe.gov.br/storage/f/2014-05-23T19%3A41%3A50.416Z/parquespracas.geojson"
+	jsondata = urlopen(urlSensor)
+	data = json.loads(jsondata.read())
 
-def getTempData(url):
-    jsondata = urlopen(url)
-    data = json.loads(jsondata.read())
-    temperatura = data["Sensor"][15]["Valor"]
-    #print temperatura
-    return temperatura
+	#print ch.encode('ascii', 'ignore')
+	retorno = []
+
+	for i in range(0,j):
+		nome = data["features"][i]["properties"]["NMNOME"]
+		nome = (unicode(nome).encode('utf8'))
+	
+		ed = data["features"][i]["properties"]["ED"]
+		ed = (unicode(ed).encode('utf8'))
+		
+		tipo = data["features"][i]["properties"]["CDTIPO"]
+		tipo = (unicode(tipo).encode('utf8'))
+		
+		area = data["features"][i]["properties"]["AREA"]
+		area = (unicode(area).encode('utf-8'))
+		
+		perimetro = data["features"][i]["properties"]["PERIMETER"]
+		perimetro = (unicode(perimetro).encode('utf-8'))
+		
+		bairroCode = data["features"][i]["properties"]["CBAIRRCODI"]
+	 	bairroCode = (unicode(bairroCode).encode('utf-8'))
+		
+		nomeBairro = data["features"][i]["properties"]["NOMEBAIRR"]
+		nomeBairro = (unicode(nomeBairro).encode('utf-8'))
+		
+		lograCode = data["features"][i]["properties"]["CLOGRACOD"]
+		lograCode =  (unicode(lograCode).encode('utf-8'))
+
+		retorno.append([nome,ed,tipo,area,perimetro,bairroCode,nomeBairro,lograCode])
+
+	return retorno
