cd94e18985c5127e6a73d38a03339fd4b03e6926
heitorrapela <hrm@cin.ufpe.br>
2016-08-14 01:03:52 -0300

    Versao dos dados locais

--- App.py
+++ App.py
@@ -1,7 +1,10 @@
 from Sensor import getEmbarqueLab
+from Sensor import getParquesPracas
 from flask import Flask, request, render_template, url_for
 import json
 
+
+##################DADOS SENSORES##########################
 # 6 - temp 7 - umidade 9 - luminosidade 17 - Pressao do ar
 listaSensores = []
 sensorAcesso = [6,7,9,17]
@@ -15,3 +18,13 @@ for x in sensorAcesso:
 #	print x[1]
 #	print x[2]
 #	print "'''"
+
+
+###################DADOS PARQUES PRACAS#################
+# 498 = Quantidade de pracas ou parques no json
+x = getParquesPracas(498)
+
+#for i in range(0,498):
+#	print x[i]
+
+###################DADOS PARQUES PRACAS#################
--- Sensor.py
+++ Sensor.py
@@ -11,3 +11,41 @@ def getEmbarqueLab(i):
 	descricao = data["Sensor"][i-1]["Descricao"]
 	valor = data["Sensor"][i-1]["Valor"]
 	return nome, descricao, valor
+
+def getParquesPracas(j):
+	urlSensor = "http://dados.recife.pe.gov.br/storage/f/2014-05-23T19%3A41%3A50.416Z/parquespracas.geojson"
+	jsondata = urlopen(urlSensor)
+	data = json.loads(jsondata.read())
+
+	#print ch.encode('ascii', 'ignore')
+	retorno = []
+
+	for i in range(0,j):
+		nome = data["features"][i]["properties"]["NMNOME"]
+		nome = (unicode(nome).encode('utf8'))
+	
+		ed = data["features"][i]["properties"]["ED"]
+		ed = (unicode(ed).encode('utf8'))
+		
+		tipo = data["features"][i]["properties"]["CDTIPO"]
+		tipo = (unicode(tipo).encode('utf8'))
+		
+		area = data["features"][i]["properties"]["AREA"]
+		area = (unicode(area).encode('utf-8'))
+		
+		perimetro = data["features"][i]["properties"]["PERIMETER"]
+		perimetro = (unicode(perimetro).encode('utf-8'))
+		
+		bairroCode = data["features"][i]["properties"]["CBAIRRCODI"]
+	 	bairroCode = (unicode(bairroCode).encode('utf-8'))
+		
+		nomeBairro = data["features"][i]["properties"]["NOMEBAIRR"]
+		nomeBairro = (unicode(nomeBairro).encode('utf-8'))
+		
+		lograCode = data["features"][i]["properties"]["CLOGRACOD"]
+		lograCode =  (unicode(lograCode).encode('utf-8'))
+
+		retorno.append([nome,ed,tipo,area,perimetro,bairroCode,nomeBairro,lograCode])
+
+	return retorno
+
